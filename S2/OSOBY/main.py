0e411f097fe205aebf87044b7e8fbc997177f9f4
albim72 <marcin.albiniak72@gmail.com>
2022-10-07 17:46:44 +0200

    Update main.py

--- S2/OSOBY/main.py
+++ S2/OSOBY/main.py
@@ -4,3 +4,11 @@ os1 = Osoba("Jan",66,176,104)
 os1.print_osoba()
 print(f"wiek za 10 lat: {os1.wiekza10lat()}")
 print(f"Czy osoba jest pracownikiem? ({os1.czypracownik()})")
+
+print("___________________________________________________")
+
+os2 = Osoba("Olga",26,170,62)
+os2.koloroczu = "niebieskie"
+os2.print_osoba()
+print(f"wiek za 10 lat: {os2.wiekza10lat()}")
+print(f"Czy osoba jest pracownikiem? ({os2.czypracownik()})")
