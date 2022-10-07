d49ba2778fe206fac04f25894e1c7881ab4f3f1a
albim72 <marcin.albiniak72@gmail.com>
2022-10-07 17:19:59 +0200

    Update main.py

--- S2/MODULY/main.py
+++ S2/MODULY/main.py
@@ -1,6 +1,14 @@
 #import dane
 #import dane as dn
 from dane import book
+from moje_funkcje.mff.funkcje12 import konferencja,gx
+from miasto import Miasto
 
 print(book)
 print(book["autor"])
+
+konferencja("Algorytmy AI - zastosowania w medycynie","Rzeszów","2022-10-11")
+print(f"wynik działania: {gx(4,5):.3f}")
+
+ms = Miasto(45,"Lublin","lubelelskie")
+ms.print_miasto()
