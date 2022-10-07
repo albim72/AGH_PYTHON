f96db29cc34f45de5a4578ca26d6bdecab989a38
albim72 <marcin.albiniak72@gmail.com>
2022-10-07 17:44:30 +0200

    Update main.py

--- S2/OSOBY/main.py
+++ S2/OSOBY/main.py
@@ -1,1 +1,6 @@
+from osoba import Osoba
 
+os1 = Osoba("Jan",66,176,104)
+os1.print_osoba()
+print(f"wiek za 10 lat: {os1.wiekza10lat()}")
+print(f"Czy osoba jest pracownikiem? ({os1.czypracownik()})")
