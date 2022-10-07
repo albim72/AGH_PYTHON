7bfc7d315d434c360d3d8b38f9605cede4c15a49
albim72 <marcin.albiniak72@gmail.com>
2022-10-07 19:42:31 +0200

    Update klasa123.py

--- S2/klasa123.py
+++ S2/klasa123.py
@@ -16,36 +16,36 @@ class DrugaKlasa(PierwszaKlasa):
 
     def print_abc(self):
         print(f"a = {self.a}, b = {self.b}, c = {self.c}")
-        
+
     def suma_elementow(self):
         return self.a + self.b + self.c
-    
+
+
 class SpecjalnaKlasa:
-    
+
     def __init__(self,g,h):
         self.g = 2*g
         self.h = h**2
-    
+
     def ghx(self):
         return (self.g+self.h)/2
-    
-    
-class TrzeciaKlasa(DrugaKlasa):
 
-    def __init__(self,a,b,c,d):
-        super().__init__(a,b,c)
+class TrzeciaKlasa(DrugaKlasa,SpecjalnaKlasa):
+
+    def __init__(self,a,b,c,d,g,h):
+        DrugaKlasa.__init__(self,a,b,c)
+        SpecjalnaKlasa.__init__(self,g,h)
         self.d=d
-        
-    def print_abcd(self):
-        print(f"a = {self.a}, b = {self.b}, c = {self.c}, d={self.d}")
+
+    def print_abcdgh(self):
+        print(f"a = {self.a}, b = {self.b}, c = {self.c}, d={self.d}, g={self.g}, h={self.h}")
 
     def suma_elementow(self):
-        return self.a + self.b + self.c + self.d
-    
-    
+        return self.a + self.b + self.c + self.d + self.ghx()
+
 pk = PierwszaKlasa(3,7)
 dk = DrugaKlasa(1,6,9)
-tk=TrzeciaKlasa(2,3,1,8)
+tk=TrzeciaKlasa(2,3,1,8,2,3)
 
 print("___________ PierwszaKlasa __________")
 pk.print_ab()
@@ -56,5 +56,8 @@ print(dk.suma_elementow())
 print("___________ TrzeciaKlasa __________")
 tk.print_ab()
 tk.print_abc()
-tk.print_abcd()
+tk.print_abcdgh()
 print(tk.suma_elementow())
+
+
+
