44f5d172f5a910859d6aa954b1d8a97c5bd5c3df
albim72 <marcin.albiniak72@gmail.com>
2022-10-17 12:50:10 +0200

    Update switch.py

--- S3/switch.py
+++ S3/switch.py
@@ -10,3 +10,23 @@ class Switch:
     
 def case(*args):
     return any((arg == Switch.value for arg in args))
+
+  
+while Switch(n):
+
+    if case(0):
+        print("n to jest wartość 0")
+        break
+    if case(1,4,9):
+        print("n jest kwadratem innej liczby")
+        break
+    if case(2):
+        print("n jest liczbą parzystą")
+    if case(2,3,5,7):
+        print("n jest liczbą pierwszą")
+        break
+    if case(6,8):
+        print("n jest krotnością liczby 2")
+        break
+    print("Pisz tylko cyfry....")
+    break
