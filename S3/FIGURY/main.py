078114db2494cc71c4bdeffd2604701ba0176cc5
albim72 <marcin.albiniak72@gmail.com>
2022-10-17 14:04:26 +0200

    Create main.py

---
+++ S3/FIGURY/main.py
@@ -0,0 +1,8 @@
+from prostokat import Prostokat
+from trojkat import Trojkat
+
+pr = Prostokat(5.5,7.3)
+tr = Trojkat(6.7,9)
+
+print(f"pole prostokąta wynosi: {pr.policz_pole():.2f}")
+print(f"pole trójkąta wynosi: {tr.policz_pole():.2f}")
