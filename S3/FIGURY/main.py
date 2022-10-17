9a6bb961ab23fc5f2b17be8a1097e4dd26912bb6
albim72 <marcin.albiniak72@gmail.com>
2022-10-17 14:25:18 +0200

    Update main.py

--- S3/FIGURY/main.py
+++ S3/FIGURY/main.py
@@ -1,8 +1,14 @@
 from prostokat import Prostokat
 from trojkat import Trojkat
+from trapez import Trapez
+from kolo import Kolo
 
 pr = Prostokat(5.5,7.3)
 tr = Trojkat(6.7,9)
+trp = Trapez(8.2,6.3,4.8)
+kl = Kolo(5.5)
 
 print(f"pole prostokąta wynosi: {pr.policz_pole():.2f}")
 print(f"pole trójkąta wynosi: {tr.policz_pole():.2f}")
+print(f"pole trapezu wynosi: {trp.policz_pole():.2f}")
+print(f"pole Koła wynosi: {kl.policz_pole():.2f}")
