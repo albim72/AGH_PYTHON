8d3285c8f4611c98031d8b2fd3d51f7495eb3379
albim72 <marcin.albiniak72@gmail.com>
2022-10-17 14:48:30 +0200

    Update pojazd.py

--- S3/KOSZTY/pojazd.py
+++ S3/KOSZTY/pojazd.py
@@ -1,9 +1,21 @@
 from ipojzad import IPojazd
+from imarka import IMarka
 
-class Pojazd(IPojazd):
+
+class Pojazd(IPojazd,IMarka):
+
+    def __init__(self,marka,model,rocznik,poj):
+        self.marka = marka
+        self.model = model
+        self.rocznik = rocznik
+        self.poj = poj
 
     def spalanie_100(self, litry, odl):
         return litry*100/odl
 
     def kosztyprzejazdu(self, litry, odl, cena_l):
         return self.spalanie_100(litry,odl)*(odl/100)*cena_l
+
+    def info_car(self):
+        return f"Opis pojazdu: {self.marka}, model: {self.model}, rok produkcji: {self.rocznik}, pojemność: {self.poj}"
+
