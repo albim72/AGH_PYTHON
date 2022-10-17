22ffa436f49132a33961b75f90601689995bdd18
albim72 <marcin.albiniak72@gmail.com>
2022-10-17 13:48:57 +0200

    Update abstrakcja.py

--- S3/abstrakcja.py
+++ S3/abstrakcja.py
@@ -15,3 +15,22 @@ class Prototyp(ABC):
     @abstractmethod
     def policz_x(self):
         return self.x*5
+    
+    
+class ZwKlasa(Prototyp):
+
+    def __init__(self,x,y):
+        super().__init__(x)
+        self.y=y
+
+    def policz(self):
+        return 7008
+
+    def policz_x(self):
+        return super().policz_x() + 4*self.y
+
+    def message(self,i):
+        return f"ważna wiadomość: {i}"
+
+
+z = ZwKlasa(4,3)
