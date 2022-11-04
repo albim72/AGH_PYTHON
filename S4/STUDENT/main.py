3227327eee109013abb07d68333bff64728f896a
albim72 <marcin.albiniak72@gmail.com>
2022-11-04 16:13:35 +0100

    Update main.py

--- S4/STUDENT/main.py
+++ S4/STUDENT/main.py
@@ -1,6 +1,20 @@
 from homework import  HomeWork
+from exam import Exam
 
 gal = HomeWork()
 gal.grade = 93
 assert gal.grade == 93
 print(f"ocena za projekt: {gal.grade}")
+
+print("________ Exam class ___________")
+
+ex = Exam()
+ex.writing_grade = 83
+ex.math_grade = 99
+
+assert ex.writing_grade == 83
+assert ex.math_grade == 99
+
+print(ex.writing_grade)
+print(ex.math_grade)
+
