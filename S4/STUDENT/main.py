515ab08f1eee9bc5fdf0f09028d1b4a123f0381d
albim72 <marcin.albiniak72@gmail.com>
2022-11-04 16:32:21 +0100

    Update main.py

--- S4/STUDENT/main.py
+++ S4/STUDENT/main.py
@@ -1,6 +1,10 @@
 from homework import  HomeWork
 from exam import Exam
 
+import grade as g
+
+
+
 gal = HomeWork()
 gal.grade = 93
 assert gal.grade == 93
@@ -18,3 +22,25 @@ assert ex.math_grade == 99
 print(ex.writing_grade)
 print(ex.math_grade)
 
+
+print("__________ klasa Gade, Exam ____________________")
+
+exam = g.Exam()
+exam.writing_grade = 40
+
+g.Exam.__dict__['writing_grade'].__set__(exam,40)
+
+#exam.writing_grade
+
+p = g.Exam.__dict__['writing_grade'].__get__(exam,g.Exam)
+
+print(p)
+
+print("__________ zmodyfikowana klasa Gade, Exam ____________________")
+
+firstexam = g.Exam()
+firstexam.writing_grade = 81
+firstexam.science_grade = 65
+
+print(f'Pisanie - ocena {firstexam.writing_grade}')
+print(f'Nauka - ocena {firstexam.science_grade}')
