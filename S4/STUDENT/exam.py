17a04629d2c5f17da7a6609b0a2a72cb452d9598
albim72 <marcin.albiniak72@gmail.com>
2022-11-04 16:13:13 +0100

    Update exam.py

--- S4/STUDENT/exam.py
+++ S4/STUDENT/exam.py
@@ -1,18 +1,18 @@
 class Exam:
-    
+
     def __init__(self):
         self._writing_grade = 0
         self._math_grade = 0
-        
+
     @staticmethod
     def _check_grade(value):
         if not (0 <= value <= 100):
             raise ValueError('ocena musi być z zakresu od 0 do 100')
-        
+
     @property
     def writing_grade(self):
         return self._writing_grade
-    
+
     @writing_grade.setter
     def writing_grade(self,value):
         self._check_grade(value)
@@ -25,4 +25,4 @@ class Exam:
     @math_grade.setter
     def math_grade(self, value):
         self._check_grade(value)
-        self._writing_grade = value
+        self._math_grade = value
