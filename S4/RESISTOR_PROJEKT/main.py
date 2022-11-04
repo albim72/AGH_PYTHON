a06baef047d23a38c9339015aaaefaa706fea0f1
albim72 <marcin.albiniak72@gmail.com>
2022-11-04 15:39:22 +0100

    Update main.py

--- S4/RESISTOR_PROJEKT/main.py
+++ S4/RESISTOR_PROJEKT/main.py
@@ -1,4 +1,6 @@
 from oldresistor import OldResistor
+from resistor import Resistor
+from voltage import VoltageResistance
 
 print("_____________________________________")
 print("klasa OldResistor")
@@ -10,3 +12,22 @@ print(r0._ohms)
 print(r0.get_ohms())
 r0.set_ohms(4.5E2)
 print(r0.get_ohms())
+
+print("_____________________________________")
+print("klasa Resistor")
+
+r1 = Resistor(50E3)
+r1.ohms = 10E3
+print(r1.ohms)
+
+print(f"układ elektryczny:\nopornik {r1.ohms} Om\nnapięcie prądu: {r1.voltage} V\n"
+      f"natężenie prądu:{r1.current} A")
+
+print("_____________________________________")
+print("klasa VoltageResistance")
+
+r2 = VoltageResistance(1.2E3)
+print(f'natężenie początkowe prądu: {r2.current} A')
+r2.voltage = 25
+print(f'natężenie prądu: {r2.current} A')
+print(f"napięcie prądu: {r2.voltage} V")
