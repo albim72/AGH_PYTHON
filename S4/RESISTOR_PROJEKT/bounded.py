51b5fe04915fe1e9340c0149773a6b7f436d8a19
albim72 <marcin.albiniak72@gmail.com>
2022-11-04 15:45:22 +0100

    Create bounded.py

---
+++ S4/RESISTOR_PROJEKT/bounded.py
@@ -0,0 +1,17 @@
+from resistor import Resistor
+
+class BoundedResistance(Resistor):
+
+    def __init__(self,ohms):
+        super().__init__(ohms)
+
+    @property
+    def ohms(self):
+        return self._ohms
+
+    @ohms.setter
+    def ohms(self,ohms):
+        if ohms <= 0:
+            raise ValueError(f'zadana wartość {ohms} Om jest niewłaściwa. Musi być  > 0')
+        self._ohms = ohms
+
