62e8f403b900240c754b0472887da062a128425e
albim72 <marcin.albiniak72@gmail.com>
2022-11-04 18:10:46 +0100

    Create main.py

---
+++ S4/DICT_ERROR/errors/main.py
@@ -0,0 +1,13 @@
+from create_dict import CustomDict
+
+test1 = CustomDict()
+print(test1)
+
+# test2 = CustomDict({'a','b'},[34,67])
+# print(test2)
+
+# test3 = CustomDict(('x','y','z'),(10,20,"trzydzieści"))
+# print(test3)
+
+test4 = CustomDict(('x','y','z'),(10,20,30))
+print(test4)
