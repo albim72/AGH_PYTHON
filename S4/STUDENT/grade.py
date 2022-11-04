936276f1f74c7cf8e19d4052ef633a7fb8be13f1
albim72 <marcin.albiniak72@gmail.com>
2022-11-04 16:27:29 +0100

    Update grade.py

--- S4/STUDENT/grade.py
+++ S4/STUDENT/grade.py
@@ -1,14 +1,19 @@
 class Grade:
-    
+
+    def __init__(self):
+        self._value = 0
+
     def __get__(self, instance, instance_type):
-        pass
-    
+        return self._value
+
     def __set__(self, instance, value):
-        pass
-    
+        if not (0 <= value <= 100):
+            raise ValueError('ocena musi być z zakresu od 0 do 100')
+        self._grade = value
+
 
 class Exam:
-    
+
     math_grade = Grade()
     writing_grade = Grade()
     science_grade = Grade()
