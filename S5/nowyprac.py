7cc2b0f4f4e79035429cf4f50a92de495db5185f
albim72 <marcin.albiniak72@gmail.com>
2022-11-25 09:39:15 +0100

    Update nowyprac.py

--- S5/nowyprac.py
+++ S5/nowyprac.py
@@ -4,4 +4,17 @@ def dodaj_nowego_pracownika(new_data,filename="pracownik.json"):
     with open(filename,"r+",encoding="utf-8") as file:
         file_data = json.load(file)
         file_data["pra_info"].append(new_data)
-        json.dumps(file_data,file,indent=4)
+        file.seek(0)
+        json.dump(file_data,file,indent=4)
+
+
+nowy_prac = {
+            "imie": "Paula",
+            "nazwisko": "Nowak",
+            "stanowisko": "sekretarka",
+            "lata_pracy": 3,
+            "email": "pnowak@firma.pl"
+        }
+
+
+dodaj_nowego_pracownika(nowy_prac)
