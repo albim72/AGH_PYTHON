2e485c0bc0b9d83cf0a93c12bfe6a329c35b3bbf
albim72 <marcin.albiniak72@gmail.com>
2022-11-25 13:02:00 +0100

    Create simplenumy.py

---
+++ S5/simplenumy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+wprowadzenie do pakietu numpy....<br>
+<img src="numpy.png" style="width:50%;">
+<h4>kilka słów o tablicach pakietu NUMPY....
+
+a = np.array([23,56,7,8,91,27,0,9,-7])
+print(a)
+
+mc = np.array([[4,6,1],[67,8,9],[-23,0,7]])
+print(mc)
