e2cef998de231cfaff9afa3c6fa86e9e8e1f42fc
albim72 <marcin.albiniak72@gmail.com>
2022-11-25 09:15:08 +0100

    Update jsonsource.py

--- S5/jsonsource.py
+++ S5/jsonsource.py
@@ -1,1 +1,12 @@
+import json
 
+x = '{"name":"Jan","age":34,"city":"Kraków","room nb":6}'
+
+print(x)
+print(type(x))
+
+osoba = json.loads(x)
+print(osoba)
+print(type(osoba))
+
+print(f"wiek osoby: {osoba['age']}")
