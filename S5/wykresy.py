89f3608c9ae009d80a076b699d9ecbf32a9e1489
albim72 <marcin.albiniak72@gmail.com>
2022-11-25 14:47:51 +0100

    Update wykresy.py

--- S5/wykresy.py
+++ S5/wykresy.py
@@ -12,3 +12,23 @@ ax.set(xlabel='czas [s]', ylabel='napięcie prądu (mV)',title='wykres napięcia
 ax.grid()
 fig.savefig("fala.png")
 plt.show()
+
+
+
+#multiple subplots
+x1 = np.linspace(0.0,5.0)
+y1 = np.cos(2*np.pi*x1)*np.exp(x1)
+
+x2 = np.linspace(0.0,2.0)
+y2 = np.cos(2*np.pi*x2)
+
+fig,(ax1,ax2) = plt.subplots(2,1)
+fig.suptitle('połączenie dwóch wykresów')
+
+ax1.plot(x1,y1,'ro-')
+ax.set_ylabel('wartość A')
+
+ax2.plot(x2,y2,'.-')
+ax.set_xlabel('czas [s]')
+ax.set_ylabel('wartość B')
+plt.show()
