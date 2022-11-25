76ac77ea38cf8808c052f8df8ff549af548b1d7e
albim72 <marcin.albiniak72@gmail.com>
2022-11-25 09:24:32 +0100

    Update jsonsource.py

--- S5/jsonsource.py
+++ S5/jsonsource.py
@@ -11,3 +11,23 @@ print(type(osoba))
 
 print(f"wiek osoby: {osoba['age']}")
 print(f"drugie imię: {osoba['names'][1]}")
+
+samochod = {
+    "marka":"Opel",
+    "model":"Insignia",
+    "poj":1.9,
+    "rocznik":2019
+}
+
+jsonsam = json.dumps(samochod,indent=4)
+print(jsonsam)
+print(type(jsonsam))
+
+with open("samochod.json","w",encoding="utf-8") as f:
+    f.write(jsonsam)
+
+
+with open("samochod.json","r",encoding="utf-8") as f:
+    auto_dict = json.load(f)
+
+print(auto_dict)
