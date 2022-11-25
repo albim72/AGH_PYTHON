8a6f8e07237190575abd7d2be48bc04f98829ddd
albim72 <marcin.albiniak72@gmail.com>
2022-11-25 10:19:11 +0100

    Update xml_source.py

--- S5/xml_source.py
+++ S5/xml_source.py
@@ -3,10 +3,10 @@ import xml.etree.ElementTree as ET
 try:
     tree = ET.parse("kraj.xml")
     root = tree.getroot()
-    
+
     for child in root:
         print(f"tag: {child.tag}, atrybuty: {child.attrib};")
-        
-        
+
+    print(root[0][1].text)
 except:
     print("element nie istnieje")
