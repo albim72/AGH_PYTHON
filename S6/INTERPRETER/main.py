4674e3663362ff93505113d54e602d6fbd3ab202
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 17:54:24 +0100

    Update main.py

--- S6/INTERPRETER/main.py
+++ S6/INTERPRETER/main.py
@@ -5,26 +5,53 @@ class AbstractExpression:
     def interpret():
         """
         ta metoda będzie wywołana rekurencyjnie dla każdego AbstractExpression
-        :return: 
+        :return:
         """
 class Number(AbstractExpression):
     def __init__(self,value):
         self.value = int(value)
-        
+
     def interpret(self):
         return self.value
-    
+
     def __repr__(self):
         return str(self.value)
-    
+
 class Add(AbstractExpression):
     def __init__(self,left,right):
         self.left = left
         self.right = right
-        
+
     def interpret(self):
         return self.left.interpret() + self.right.interpret()
-    
+
     def __repr__(self):
         return f"({self.left} Add {self.right})"
-    
+
+class Substract(AbstractExpression):
+    def __init__(self,left,right):
+        self.left = left
+        self.right = right
+
+    def interpret(self):
+        return self.left.interpret() - self.right.interpret()
+
+    def __repr__(self):
+        return f"({self.left} Substract {self.right})"
+
+
+SENTENCE = "5 + 4 - 13 + 17 - 2"
+print(SENTENCE)
+
+TOKENS = SENTENCE.split(" ")
+print(TOKENS)
+
+AST:list[AbstractExpression] = []
+AST.append(Add(Number(TOKENS[0]),Number(TOKENS[2]))) #5 + 4
+AST.append(Substract(AST[0],Number(TOKENS[4]))) #^ - 13
+AST.append(Add(AST[1],Number(TOKENS[6]))) #^ + 17
+AST.append(Substract(AST[2],Number(TOKENS[8])))#^ -2
+
+AST_ROOT = AST.pop()
+print(AST_ROOT.interpret())
+print(AST_ROOT)
