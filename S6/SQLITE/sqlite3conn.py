938c61cc62c61cbd0b402e8f123a3062c4851ad2
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 15:04:22 +0100

    Create sqlite3conn.py

---
+++ S6/SQLITE/sqlite3conn.py
@@ -0,0 +1,16 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as a:
+        print(a)
+    finally:
+        if conn:
+            conn.close()
+
+if __name__ == '__main__':
+    create_connection(r"c:\sqlite\db\ptyhoncqlite.db")
