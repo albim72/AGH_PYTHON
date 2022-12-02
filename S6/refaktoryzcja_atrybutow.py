599c2f8890b78834c2781a836959ac75fb7c169d
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 18:39:38 +0100

    Update refaktoryzcja_atrybutow.py

--- S6/refaktoryzcja_atrybutow.py
+++ S6/refaktoryzcja_atrybutow.py
@@ -47,5 +47,39 @@ print("***************************************")
 print("nowe spojrzenie na wiadro....")
 
 
+class NewBucket:
+    def __init__(self,period):
+        self.period_delta = timedelta(seconds=period)
+        self.reset_time = datetime.now()
+        self.max_quota = 0
+        self.quota_consumed = 0
 
+    def __repr__(self):
+        return (f'NewBucket(max_quota={self.max_quota}, quota_consumed: {self.quota_consumed})')
+
+    @property
+    def quota(self):
+        return self.max_quota-self.quota_consumed
+
+    @quota.setter
+    def quota(self,amount):
+        delta = self.max_quota - amount
+        if amount == 0:
+            #liczba jednostek danych jest wyzerowana
+            self.quota_consumed = 0
+            self.max_quota = 0
+        elif delta < 0:
+            #liczba jednostek danych przygotowana dla nowego przedziału czasu
+            assert self.quota_consumed == 0
+            self.max_quota = amount
+        else:
+            #liczba jednostek danych wykorzystana w przedziale czasu
+
+            assert self.max_quota >= self.quota_consumed
+            self.quota_consumed+=delta
+
+bucket = NewBucket(60)
+print(f"początkowo {bucket}")
+fill(bucket,100)
+print(f'wypełniono: {bucket}')
 
