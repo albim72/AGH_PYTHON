9e7b6f555b9ed9bcbeb56972246b357beafeee86
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 15:08:58 +0100

    Create tables_sqlite.py

---
+++ S6/SQLITE/tables_sqlite.py
@@ -0,0 +1,18 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as a:
+        print(a)
+    return conn
+
+def create_table(conn,create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
