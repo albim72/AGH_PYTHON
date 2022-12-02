3ad1e80c4693a6bcee41d490707c91a74996cb10
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 15:18:07 +0100

    Update tables_sqlite.py

--- S6/SQLITE/tables_sqlite.py
+++ S6/SQLITE/tables_sqlite.py
@@ -28,7 +28,7 @@ def main():
     end_date text
     );
     """
-    
+
     sql_create_tasks_table = """
     CREATE TABLE IF NOT EXISTS tasks(
     id integer PRIMARY KEY,
@@ -41,3 +41,13 @@ def main():
     FOREIGN KEY (project_id) REFERENCES projects(id)
     );
     """
+
+    conn = create_connection(database)
+    if conn is not None:
+        create_table(conn,sql_create_projects_table)
+        create_table(conn,sql_create_tasks_table)
+    else:
+        print("Błąd! nie można utworzyć połączenia z bazą!")
+
+if __name__ == '__main__':
+    main()
