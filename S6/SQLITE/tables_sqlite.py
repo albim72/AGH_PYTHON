e0ae48588753df2b6ef5af055a4a4a4c59a73dd7
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 15:15:32 +0100

    Update tables_sqlite.py

--- S6/SQLITE/tables_sqlite.py
+++ S6/SQLITE/tables_sqlite.py
@@ -16,3 +16,28 @@ def create_table(conn,create_table_sql):
         c.execute(create_table_sql)
     except Error as e:
         print(e)
+
+def main():
+    database = r"c:\sqlite\db\ptyhoncqlite.db"
+
+    sql_create_projects_table = """
+    CREATE TABLE IF NOT EXISTS projects(
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    begin_date text,
+    end_date text
+    );
+    """
+    
+    sql_create_tasks_table = """
+    CREATE TABLE IF NOT EXISTS tasks(
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    priority integer,
+    status_id integer NOT NULL,
+    project_id integer NOT NULL,
+    begin_date text,
+    end_date text,
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+    );
+    """
