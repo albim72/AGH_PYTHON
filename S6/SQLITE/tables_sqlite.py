6be10d17cd29f5ff46f3a550209019fd27c5a6c8
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 15:33:42 +0100

    Update tables_sqlite.py

--- S6/SQLITE/tables_sqlite.py
+++ S6/SQLITE/tables_sqlite.py
@@ -14,7 +14,7 @@ def create_project(conn,project):
     sql = """
     INSERT INTO projects(name,begin_date,end_date) VALUES(?,?,?)
     """
-    
+
     cur = conn.cursor()
     cur.execute(sql,project)
     conn.commit()
@@ -30,3 +30,20 @@ def create_task(conn,task):
     cur.execute(sql, task)
     conn.commit()
     return cur.lastrowid
+
+def main():
+    database = r"c:\sqlite\db\ptyhoncqlite.db"
+
+    conn = create_connection(database)
+
+    with conn:
+        project = ('Super Apka napisana w Pythonie','2021-02-02','2022-10-12')
+        project_id = create_project(conn,project)
+        task_1 = ('Analiza wymagań dotyczących aplikacji',1,1,project_id,'2021-02-08','2021-04-10')
+        task_2 = ('panel dyskusyjny z klientem',1,1,project_id,'2021-03-21','2021-05-13')
+
+        create_task(conn,task_1)
+        create_task(conn,task_2)
+
+if __name__ == '__main__':
+    main()
