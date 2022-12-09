3bdf642a83a18b017b4b6ae47aab49a4045864ea
albim72 <marcin.albiniak72@gmail.com>
2022-12-09 14:23:15 +0100

    Create queue_stack.py

---
+++ S7/queue_stack.py
@@ -0,0 +1,30 @@
+queue = []
+queue.append('mors')
+queue.append('foka')
+queue.append('orka')
+
+print("inicjacja kolejki")
+print(queue)
+print("\n elementy zdjęte z kolejki")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print("\n kolejka po zdjęciu elementów")
+print(queue)
+
+#analiza stosu
+print("_________________________")
+stack = []
+stack.append("buldog angielski")
+stack.append("wyżeł weimarski")
+stack.append("york")
+print("inicjacja stosu")
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print("\n kolejka po zdjęciu stosu")
+print(stack)
+
