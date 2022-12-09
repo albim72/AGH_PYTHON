0d59295e7a8c5cd9a0a4e12a085f7f10abd27f5b
albim72 <marcin.albiniak72@gmail.com>
2022-12-09 15:00:34 +0100

    Update main.py

--- S7/PRIME_NUMBERS/main.py
+++ S7/PRIME_NUMBERS/main.py
@@ -3,7 +3,7 @@ import time,concurrent.futures
 def find_prime_numbers_sum(minimum,maximum):
     total = 0
     for number in range(minimum,maximum+1):
-        count =0 
+        count =0
         for i in range(2,(number//2+1)):
             if number%i==0:
                 count=count+1
@@ -11,3 +11,19 @@ def find_prime_numbers_sum(minimum,maximum):
         if count==0 and number!=1:
             total = total + number
     return total
+
+def run_heavy_function(params):
+    return find_prime_numbers_sum(*params)
+
+def main():
+    numbers = [(1,10000),(3,50000),(5000,100000),(4,900),(800,15000),(355,1890)]
+    start_time = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        result = executor.map(run_heavy_function,numbers)
+
+    print(list(result))
+    end_time = time.time()
+    print(f'Całkowity czas wykonania wątków asynchronicznych: {end_time - start_time:2f} s')
+
+if __name__ == '__main__':
+    main()
