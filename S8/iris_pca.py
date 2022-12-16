4ee09be505b396d8643c1e18534b1eff8f3de637
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 12:50:39 +0100

    Update iris_pca.py

--- S8/iris_pca.py
+++ S8/iris_pca.py
@@ -18,3 +18,18 @@ pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x)
 principalDf = pd.DataFrame(data=principalComponents, columns=['principal component 1', 'principal component 2'])
 finalDf = pd.concat([principalDf,df[['target']]],axis=1)
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('principal component 1',fontsize=15)
+ax.set_ylabel('principal component 2',fontsize=15)
+ax.set_title('dwa komponenty PCA',fontsize=20)
+targets = ['Iris-setosa','Iris-versicolor','Iris-virginica']
+colors = ['r','g','b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep,'principal component 1'],
+               finalDf.loc[indicesToKeep,'principal component 2'],
+               c=color,
+               s=50)
+ax.legend(targets)
+ax.grid()
