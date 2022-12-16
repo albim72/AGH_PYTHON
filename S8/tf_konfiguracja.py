f610172e1e2f4808e22b60d381f0a49f6ea49324
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 10:40:47 +0100

    Update tf_konfiguracja.py

--- S8/tf_konfiguracja.py
+++ S8/tf_konfiguracja.py
@@ -16,3 +16,19 @@ predictions = model(x_train[:1]).numpy()
 predictions
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 loss_fn(y_train[:1],predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train,y_train,epochs=5)
+
+
+model.evaluate(x_test,y_test,verbose=2)
+
+probability_model = tf.keras.Sequential([
+    model,
+    tf.keras.layers.Softmax()
+])
+
+probability_model(x_test[:5])
