a7f33e4fe5f297a643d7bc4bbab7d0861c04c919
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 12:17:37 +0100

    Update klasyfikator_irysy.py

--- S8/klasyfikator_irysy.py
+++ S8/klasyfikator_irysy.py
@@ -6,3 +6,9 @@ import matplotlib.pyplot as plt
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 df = pd.read_csv(url)
 df.head()
+
+
+X = df.iloc[:,1:4].values
+y = df.iloc[:,4].values
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25)
