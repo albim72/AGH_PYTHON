1b1bf84467aa5e43dcb0209ca305868d8125e3c4
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 12:21:30 +0100

    Update klasyfikator_irysy.py

--- S8/klasyfikator_irysy.py
+++ S8/klasyfikator_irysy.py
@@ -16,3 +16,8 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25)
 from sklearn.linear_model import  LogisticRegression
 classifier = LogisticRegression()
 classifier.fit(X_train,y_train)
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
