13af6526fca457a289d61df4b8ef4b0e6d9fb86b
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 11:59:40 +0100

    Update klasyfikacja_moda.py

--- S8/klasyfikacja_moda.py
+++ S8/klasyfikacja_moda.py
@@ -95,3 +95,15 @@ plt.subplot(1,2,2)
 plot_value_array(i,predictions[i],test_labels)
 plt.show()
 
+num_rows = 5
+num_cols = 3
+
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols,2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows,2*num_cols,2*i+1)
+    plot_image(i,predictions[i],test_labels,test_images)
+    plt.subplot(num_rows,2*num_cols,2*i+2)
+    plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
