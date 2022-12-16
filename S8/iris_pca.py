21ebb23cef3644ba9fcca6ba28eb124befc0eb46
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 12:36:14 +0100

    Update iris_pca.py

--- S8/iris_pca.py
+++ S8/iris_pca.py
@@ -6,3 +6,9 @@ import matplotlib.pyplot as plt
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 df = pd.read_csv(url,names=['sepal length','sepal width','petal length','petal width','target'])
 df.head()
+
+from sklearn.preprocessing import StandardScaler
+features = ['sepal length','sepal width','petal length','petal width']
+x = df.loc[:,features].values
+y = df.loc[:,['target']].values
+x = StandardScaler().fit_transform(x)
