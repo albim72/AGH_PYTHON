4662b91e99ce32400496ec905ab59e5e0d1cfcb6
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 13:32:53 +0100

    Update samochody.py

--- S8/samochody.py
+++ S8/samochody.py
@@ -10,3 +10,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data"
+column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight','Acceleration','Model Year','Origin']
+
+raw_dataset = pd.read_csv(url,names=column_names,na_values="?",comment="\t", sep=" ",skipinitialspace=True)
