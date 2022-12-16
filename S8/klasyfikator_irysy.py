40c71513111b3305f9a49cba8467649d50dad6b5
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 12:14:15 +0100

    Update klasyfikator_irysy.py

--- S8/klasyfikator_irysy.py
+++ S8/klasyfikator_irysy.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+df = pd.read_csv(url)
+df.head()
