feb83c63d5bef2c9e578c6f6bf7789ed373d0c94
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 11:14:59 +0100

    Update klasyfikacja_moda.py

--- S8/klasyfikacja_moda.py
+++ S8/klasyfikacja_moda.py
@@ -8,3 +8,4 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 class_names = ['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
 train_images.shape
 len(train_labels)
+test_images.shape
