18a4689d1f0de200464286677f596523076f0596
albim72 <marcin.albiniak72@gmail.com>
2022-12-16 11:11:08 +0100

    Update klasyfikacja_moda.py

--- S8/klasyfikacja_moda.py
+++ S8/klasyfikacja_moda.py
@@ -1,3 +1,6 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images,train_labels),(test_images,test_labels) = fashion_mnist.load_data()
