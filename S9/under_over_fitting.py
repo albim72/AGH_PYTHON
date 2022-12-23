1fdf2c070a820ccccfa2cf4c0c1b6fe7d98c0b5f
albim72 <marcin.albiniak72@gmail.com>
2022-12-23 11:30:56 +0100

    Create under_over_fitting.py

---
+++ S9/under_over_fitting.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+
+!pip install git+https://github.com/tensorflow/docs
+
+import tensorflow_docs as tfdocs
+import tensorflow_docs.modelling
+import tensorflow_docs.plots
+
+
+from IPython import display
+from matplotlib impoer pyplot as plt
+import numpy as np
+import pathlib
+import shutil
+import tempfile
