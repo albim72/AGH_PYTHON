901484556bac6aa77c2869d352f6c3202649210c
albim72 <marcin.albiniak72@gmail.com>
2022-12-23 11:36:53 +0100

    Update under_over_fitting.py

--- S9/under_over_fitting.py
+++ S9/under_over_fitting.py
@@ -19,3 +19,5 @@ import tempfile
 
 logdir = pathlib.Path(tempfile.mkdtemp())/"tensorboard_logs"
 shutil.rmtree(logdir, ignore_errors=True)
+
+gz = tf.keras.utils.get_file('HIGGS.csv.gz','http://mlphysics.ics.uci.edu/data/higgs/HIGGS.csv.gz')
