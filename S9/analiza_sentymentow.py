60af6ce4a7b1e56ef1f53e617085547e41647e52
albim72 <marcin.albiniak72@gmail.com>
2022-12-23 10:09:35 +0100

    Update analiza_sentymentow.py

--- S9/analiza_sentymentow.py
+++ S9/analiza_sentymentow.py
@@ -60,3 +60,16 @@ def custom_standarization(input_data):
   lowercase = tf.strings.lower(input_data)
   stripped_html = tf.strings.regex_replace(lowercase,'<br />',' ')
   return tf.strings.regex_replace(stripped_html,'[%s]' %re.escape(string.punctuation),'')
+
+from tensorflow.python.ops.array_ops import sequence_mask
+from prompt_toolkit import output
+#tworzenie warstwy wektoryzacji
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = layers.TextVectorization(
+    standardize = custom_standarization,
+    max_tokens = max_features,
+    output_mode = 'int',
+    output_sequence_length = sequence_length
+)
