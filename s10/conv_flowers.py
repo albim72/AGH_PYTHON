ec0c5db0263b05526ffc77b196c693931b54403b
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 13:33:24 +0100

    Update conv_flowers.py

--- s10/conv_flowers.py
+++ s10/conv_flowers.py
@@ -49,3 +49,16 @@ class_names = train_ds.class_names
 print(class_names)
 
 import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3,3,i+1)
+    plt.imshow(images[i].numpy().astype('uint8'))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+    
+for image_batch, labels_batch in train_ds:
+  print(image_batch.shape)
+  print(labels_batch.shape)
+  break
