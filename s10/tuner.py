b547bb00a805ac358ebb2f2fd1a08582c1e8a287
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 15:36:07 +0100

    Update tuner.py

--- s10/tuner.py
+++ s10/tuner.py
@@ -4,3 +4,6 @@ from tensorflow import keras
 import keras_tuner as kt
 
 (img_train, label_train), (img_test,label_test)= keras.datasets.fashion_mnist.load_data()
+from tensorflow.python import train
+img_train = img_train.astype('float32')/255.0
+img_test = img_test.astype('float32')/255.0
