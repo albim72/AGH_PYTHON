914d41fba08b3cd91f230e7ef2b0dc8c59ad97be
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 13:42:01 +0100

    Update conv_flowers.py

--- s10/conv_flowers.py
+++ s10/conv_flowers.py
@@ -31,8 +31,8 @@ img_width = 180
 train_ds = tf.keras.utils.image_dataset_from_directory(
     data_dir,
     validation_split = 0.2,
-    subset = 'training',
-    seed = 123,
+    subset = 'training',  
+    seed = 123,                          
     image_size = (img_height, img_width),
     batch_size=batch_size
 )
@@ -68,3 +68,8 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 normalization_layer = layers.Rescaling(1./255)
+from tensorflow.python.ops.math_ops import imag_eager_fallback
+normalized_ds = train_ds.map(lambda x,y: (normalization_layer(x),y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+print(np.min(first_image), np.max(first_image))
