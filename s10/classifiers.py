c5951a68fc5ac7b54348033be41f39ca4240e89c
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 08:55:09 +0100

    Update classifiers.py

--- s10/classifiers.py
+++ s10/classifiers.py
@@ -70,12 +70,46 @@ for ds_cnt,ds in enumerate(datasets):
         ax.set_title("Input data")
     #rysowanie punktów treningowych
     ax.scatter(X_train[:,0],X_train[:,1],c=y_train,cmap=cm_bright,edgecolor="k")
-    
+
     #rysowanie punktów testowych
     ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6, edgecolor="k")
-    
+
     ax.set_xlim(x_min, x_max)
     ax.set_ylim(y_min, y_max)
     ax.set_xticks(())
     ax.set_yticks(())
     i+=1
+
+    #iteracja po klasyfikatorach
+    for name,clf in zip(names,classifiers):
+        ax = plt.subplot(len(datasets),len(classifiers)+1,i)
+        
+        clf = make_pipeline(StandardScaler(),clf)
+        clf.fit(X_train,y_train)
+        score = clf.score(X_test,y_test)
+        DecisionBoundaryDisplay.from_estimator(clf,X,cmap=cm,alpha=0.8,ax=ax,eps=0.5)
+
+        # rysowanie punktów treningowych
+        ax.scatter(X_train[:,0],X_train[:,1],c=y_train,cmap=cm_bright,edgecolor="k")
+
+        # rysowanie punktów testowych
+        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6, edgecolor="k")
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(y_min, y_max)
+        ax.set_xticks(())
+        ax.set_yticks(())
+        
+        if ds_cnt == 0:
+            ax.set_title(name)
+        ax.text(
+            x_max - 0.3,
+            y_min + 0.3,
+            ("%.2f" %score).lstrip("0"),
+            size = 15,
+            horizontalalignment = "right",
+        )
+        i+=1
+        
+plt.tight_layout()
+plt.show()
+        
