bf35cfc72837b9c4df83dc0b757a73e5725b6a23
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 10:26:20 +0100

    Create neighbours.py

---
+++ s10/neighbours.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier, NeighborhoodComponentsAnalysis
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
