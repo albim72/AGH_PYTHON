ca83b8045345c75f24117f6c02c9f103a90af005
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 13:04:57 +0100

    Update conv_flowers.py

--- s10/conv_flowers.py
+++ s10/conv_flowers.py
@@ -9,4 +9,7 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 
-
+import pathlib
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flowers_photos',origin=dataset_url,untar=True)
+data_dir = pathlib.Path(data_dir)
