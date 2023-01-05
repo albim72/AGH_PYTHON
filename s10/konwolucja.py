de4855925fb4ee09b6c91c3937579b74dc0e13de
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 12:28:51 +0100

    Update konwolucja.py

--- s10/konwolucja.py
+++ s10/konwolucja.py
@@ -5,3 +5,14 @@ import matplotlib.pyplot as plt
 (train_images, train_labels),(test_images, test_labels) = datasets.cifar10.load_data()
 #normalizacja wartości pikseli
 train_images, test_images = train_images/255.0, test_images/255.0
+
+class_names =['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
