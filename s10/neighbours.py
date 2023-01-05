c86e0db0ee672950feed14f83da3a053279013f2
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 10:33:09 +0100

    Update neighbours.py

--- s10/neighbours.py
+++ s10/neighbours.py
@@ -7,3 +7,21 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.neighbors import KNeighborsClassifier, NeighborhoodComponentsAnalysis
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+
+n_neighbours = 3
+random_state = 0
+
+X,y = datasets.load_digits(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,y,test_size=0.5, stratify=y,random_state=random_state
+)
+
+dim = len(X[0])
+n_classes = len(np.unique(y))
+
+pca = make_pipeline(StandardScaler(),PCA(n_components=2,random_state=random_state))
+
+lda = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis(n_components=2))
+
+nca = make_pipeline(StandardScaler(), NeighborhoodComponentsAnalysis(n_components=2,random_state=random_state))
