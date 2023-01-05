a7359768491d68b656bc45e327eda7bac5ebf8c6
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 12:39:02 +0100

    Update konwolucja.py

--- s10/konwolucja.py
+++ s10/konwolucja.py
@@ -26,3 +26,9 @@ model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(64,(3,3),activation='relu'))
 
 model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64,activation='relu'))
+model.add(layers.Dense(10))
+
+model.summary()
