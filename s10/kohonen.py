45fe1a7bfda50e14b3b35440bfa665ea3e77e96b
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 10:58:23 +0100

    Update kohonen.py

--- s10/kohonen.py
+++ s10/kohonen.py
@@ -1,4 +1,8 @@
 import numpy as np
 import pandas as p
+import pandas as pd
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal length','sepal width','petal length','petal width','class']
+
+ds = pd.read_csv(url,names=names)
