3df0e9ce7b1c4aa471b12fdabc8c29cca9bd2581
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 15:43:48 +0100

    Update tuner.py

--- s10/tuner.py
+++ s10/tuner.py
@@ -7,3 +7,19 @@ import keras_tuner as kt
 from tensorflow.python import train
 img_train = img_train.astype('float32')/255.0
 img_test = img_test.astype('float32')/255.0
+
+
+def model_builder(hp):
+  model = keras.Sequential()
+  model.add(keras.layers.Flatten(input_shape=(28,28)))
+  hp_units = hp.Int('units',min_value=32,max_value=512,step=32)
+  model.add(keras.layers.Dense(units=hp_units,activation='relu'))
+  model.add(keras.layers.Dense(10))
+
+  hp_learning_rate = hp.Choice('learning_rate',values=[1e-2,1e-3,1e-4])
+
+  model.compile(optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
+                loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+  return model
+
