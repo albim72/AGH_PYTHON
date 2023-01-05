c62b7675738acff7cd71bbc434d15c522fb96ca7
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 14:11:12 +0100

    Update conv_flowers.py

--- s10/conv_flowers.py
+++ s10/conv_flowers.py
@@ -73,3 +73,19 @@ normalized_ds = train_ds.map(lambda x,y: (normalization_layer(x),y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
 print(np.min(first_image), np.max(first_image))
+
+num_classes = len(class_names)
+print(num_classes)
+
+model = Sequential([
+    layers.Rescaling(1./255, input_shape=(img_height,img_width,3)),
+    layers.Conv2D(16,3,padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32,3,padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64,3,padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128,activation='relu'),
+    layers.Dense(num_classes)
+])
