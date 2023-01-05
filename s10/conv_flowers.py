f5a753060d85f927b2e9373a8cd5f6bb29e1be6d
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 13:00:46 +0100

    Create conv_flowers.py

---
+++ s10/conv_flowers.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+
+
