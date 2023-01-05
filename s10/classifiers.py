5892702bd4c92ae844da8949fc6723f248caa614
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 08:33:30 +0100

    Update classifiers.py

--- s10/classifiers.py
+++ s10/classifiers.py
@@ -41,3 +41,14 @@ classifiers = [
     GaussianNB(),
     QuadraticDiscriminantAnalysis(),
 ]
+
+X,y = make_classification(n_features=2,n_informative=2,random_state=1,n_clusters_per_class=1)
+rng = np.random.RandomState(2)
+X+= 2*rng.uniform(size=X.shape)
+lineary_separable = (X,y)
+
+datasets = [
+    make_moons(noise=0.3, random_state=0),
+    make_circles(noise=0.2,factor=0.5,random_state=1),
+    lineary_separable
+]
