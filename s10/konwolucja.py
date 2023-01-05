df81c355de6fd9f7dc08bdf84a69fd5d30281d5e
albim72 <marcin.albiniak72@gmail.com>
2023-01-05 12:36:03 +0100

    Update konwolucja.py

--- s10/konwolucja.py
+++ s10/konwolucja.py
@@ -16,3 +16,13 @@ for i in range(25):
     plt.imshow(train_images[i])
     plt.xlabel(class_names[train_labels[i][0]])
 plt.show()
+
+#tworzenie konwolucyjnego modelu sieci neuronowej
+model = models.Sequential()
+model.add(layers.Conv2D(32,(3,3),activation='relu', input_shape = (32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+
+model.summary()
