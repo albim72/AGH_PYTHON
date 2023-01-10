6041fbd60bb0167b46888cb8ab5322af28617592
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 18:34:54 +0100

    Update GAN.py

--- S11/GAN.py
+++ S11/GAN.py
@@ -83,3 +83,5 @@ def discriminator_loss(real_output,fake_output):
 
 def generator_loss(fake_output):
   return cross_entropy(tf.ones_like(fake_output),fake_output)
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
