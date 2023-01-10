0495fc0108bd7057b724da37628729c7d1aa417f
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 18:51:31 +0100

    Update GAN.py

--- S11/GAN.py
+++ S11/GAN.py
@@ -98,3 +98,20 @@ num_examples_to_generate = 16
 
 seed = tf.random.normal([num_examples_to_generate,noise_dim])
 
+@tf.function
+def train_step(images):
+  noise = tf.random.normal([BATCH_SIZE,noise_dim])
+
+  with tf.GradientTape() as gen_type, tf.GradientTape() as disc_type:
+    generated_images = generator(noise,training=True)
+
+    real_output = discriminator(images,training=True)
+    fakeoutput = discriminator(generated_images,training = True)
+
+    gen_loss = generator_loss(fake_output)
+    disc_loss = discriminator_loss(real_output,fake_output)
+  gradients_of_generator = gen_type.gradient(gen_loss,generator.trainable_variables)
+  gradients_of_discriminator = gen_type.gradient(disc_loss,discriminator.trainable_variables)
+
+  generator_optimizer.apply_gradients(zip(gradients_of_generator,generator.trainable_variables))
+  generator_optimizer.apply_gradients(zip(gradients_of_discriminator,discriminator.trainable_variables))
