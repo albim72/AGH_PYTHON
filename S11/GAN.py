55a28c6c05ae83d42576d07781d333d8dceee8a3
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 18:24:26 +0100

    Update GAN.py

--- S11/GAN.py
+++ S11/GAN.py
@@ -53,3 +53,20 @@ generator = make_generator_model()
 noise = tf.random.normal([1,100])
 generated_image = generator(noise,training=False)
 plt.imshow(generated_image[0, :, :, 0], cmap = 'gray')
+
+#dyskryminator
+
+def make_discriminator_model():
+  model = tf.keras.Sequential()
+  model.add(layers.Conv2D(64,(5,5), strides = (2,2), padding='same', input_shape=[28,28,1]))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Conv2D(128,(5,5), strides = (2,2), padding='same'))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Flatten())
+  model.add(layers.Dense(1))
+
+  return model
