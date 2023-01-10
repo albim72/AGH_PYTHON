5be5caf64dc34df8a4b725d93bf8130a90d2c5a0
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 16:23:57 +0100

    Update pkontrolne.py

--- S11/pkontrolne.py
+++ S11/pkontrolne.py
@@ -10,3 +10,17 @@ test_labels = test_labels[:1000]
 train_images = train_images[:1000].reshape(-1,28*28)/255.0
 test_images = test_images[:1000].reshape(-1,28*28)/255.0
 
+def create_model():
+  model = tf.keras.models.Sequential([
+      keras.layers.Dense(512,activation='relu',input_shape=(784,)),
+      keras.layers.Dropout(0.2),
+      keras.layers.Dense(10)
+  ])
+
+  model.compile(optimizer='adam',
+                loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=[tf.metrics.SparseCategoricalAccuracy()])
+  return model
+
+model = create_model()
+model.summary()
