b88c840cf69f9168dc88415101e360484c590e12
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 18:15:31 +0100

    Update GAN.py

--- S11/GAN.py
+++ S11/GAN.py
@@ -23,3 +23,27 @@ BUFFER_SIZE = 60000
 BATCH_SIZE = 256
 
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+def make_generator_model():
+  model = tf.keras.Sequential()
+  model.add(layers.Dense(7*7*256, use_bias = False, input_shape=(100,)))
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Reshape((7,7,256)))
+  assert model.output_shape == (None,7,7,256)
+
+  model.add(layers.Conv2DTranspose(128,(5,5),strides=(1,1), padding='same',use_bias=False))
+  assert model.output_shape == (None,7,7,128)
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(64,(5,5),strides=(2,2), padding='same',use_bias=False))
+  assert model.output_shape == (None,14,14,64)
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(1,(5,5),strides=(2,2), padding='same',use_bias=False, activation='tanh'))
+  assert model.output_shape == (None,28,28,1)
+
+  return model
