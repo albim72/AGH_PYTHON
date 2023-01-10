01406f5b0e2588a8860342d03c6a5e9030213949
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 18:03:58 +0100

    Update GAN.py

--- S11/GAN.py
+++ S11/GAN.py
@@ -16,3 +16,8 @@ import time
 from IPython import display
 
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0],28,28,1).astype('float32')
+train_images = (train_images-127.5)/127.5
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
