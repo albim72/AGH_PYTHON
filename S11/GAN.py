012856ea847d257efb86b70a38af81250e3ad3e7
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 18:28:11 +0100

    Update GAN.py

--- S11/GAN.py
+++ S11/GAN.py
@@ -73,3 +73,5 @@ def make_discriminator_model():
 discriminator = make_discriminator_model()
 decision = discriminator(generated_image)
 print(decision)
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
