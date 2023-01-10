ef26b9fe5d51e68469020093d7390932b3cf171d
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 20:35:49 +0100

    Update onemax_ga.py

--- S11/onemax_ga.py
+++ S11/onemax_ga.py
@@ -1,88 +1,146 @@
 import random
-import deap
+
 from deap import base
 from deap import creator
 from deap import tools
 
-creator.create("FitnessMax",base.Fitness, weights=(1.0,))
-creator.create("Individual",list,fitness=creator.FitnessMax)
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 
-toolbox.register("attr_bool",random.randint,0,1)
-toolbox.register("individual",tools.initRepeat,creator.Individual,toolbox.attr_bool,100)
+# Attribute generator
+#                      define 'attr_bool' to be an attribute ('gene')
+#                      which corresponds to integers sampled uniformly
+#                      from the range [0,1] (i.e. 0 or 1 with equal
+#                      probability)
+toolbox.register("attr_bool", random.randint, 0, 1)
 
-toolbox.register("population",tools.initRepeat,list,toolbox.individual)
+# Structure initializers
+#                         define 'individual' to be an individual
+#                         consisting of 100 'attr_bool' elements ('genes')
+toolbox.register("individual", tools.initRepeat, creator.Individual,
+                 toolbox.attr_bool, 100)
 
+# define the population to be a list of individuals
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+
+# the goal ('fitness') function to be maximized
 def evalOneMax(individual):
-    return sum(individual)
+    return sum(individual),
 
-#rejestracja operatorów selekcji
 
-toolbox.register("evaluate",evalOneMax)
-toolbox.register("mate",tools.cxTwoPoint)
-toolbox.register("mutate",tools.mutFlipBit,indpb=0.05)
-toolbox.register("select",tools.selTournament,tournsize = 3)
+# ----------
+# Operator registration
+# ----------
+# register the goal / fitness function
+toolbox.register("evaluate", evalOneMax)
+
+# register the crossover operator
+toolbox.register("mate", tools.cxTwoPoint)
+
+# register a mutation operator with a probability to
+# flip each attribute/gene of 0.05
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+
+# operator for selecting individuals for breeding the next
+# generation: each individual of the current generation
+# is replaced by the 'fittest' (best) of three individuals
+# drawn randomly from the current generation.
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+
+# ----------
 
 def main():
     random.seed(64)
-    pop = toolbox.population(n=300)
-    CXPB,MUTPB = 0.5,0.2
-    print("Początek ewolucji....")
 
-    fitness = list(map(toolbox.evaluate,pop))
-    for ind,fit in zip(pop,fitness):
+    # create an initial population of 300 individuals (where
+    # each individual is a list of integers)
+    pop = toolbox.population(n=300)
+
+    # CXPB  is the probability with which two individuals
+    #       are crossed
+    #
+    # MUTPB is the probability for mutating an individual
+    CXPB, MUTPB = 0.5, 0.2
+
+    print("Start of evolution")
+
+    # Evaluate the entire population
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
 
-    print(f"Proces ewaluacji {pop} osobników")
-    fits = [ind.fitness.values[0] for ind in pop]
-    g=0
-    while max(fits)<100 and g < 1000:
-        g=g+1
-        print(f"--- generacja {g} -----")
+    print("  Evaluated %i individuals" % len(pop))
 
-        offspring = toolbox.select(pop,len(pop))
+    # Extracting all the fitnesses of
+    fits = [ind.fitness.values[0] for ind in pop]
+
+    # Variable keeping track of the number of generations
+    g = 0
+
+    # Begin the evolution
+    while max(fits) < 100 and g < 1000:
+        # A new generation
+        g = g + 1
+        print("-- Generation %i --" % g)
+
+        # Select the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        # Clone the selected individuals
         offspring = list(map(toolbox.clone, offspring))
 
+        # Apply crossover and mutation on the offspring
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            if random.random() < CXPB:
-                toolbox.mate(child1,child2)
 
+            # cross two individuals with probability CXPB
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+
+                # fitness values of the children
+                # must be recalculated later
                 del child1.fitness.values
                 del child2.fitness.values
 
         for mutant in offspring:
+
+            # mutate an individual with probability MUTPB
             if random.random() < MUTPB:
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
 
-
+        # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitness = map(toolbox.evaluate, invalid_ind)
-
-        for ind,fit in zip(invalid_ind,fitness):
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
 
-        print(f"oceniono {invalid_ind} osobników")
+        print("  Evaluated %i individuals" % len(invalid_ind))
 
+        # The population is entirely replaced by the offspring
         pop[:] = offspring
 
+        # Gather all the fitnesses in one list and print the stats
         fits = [ind.fitness.values[0] for ind in pop]
 
         length = len(pop)
-        mean = sum(fits)/length
-        sum2 = sum(x*x for x in fits)
-        std = abs(sum2/length-mean**2)**0.5
+        mean = sum(fits) / length
+        sum2 = sum(x * x for x in fits)
+        std = abs(sum2 / length - mean ** 2) ** 0.5
 
-        print(f" Min {min(fits)}")
-        print(f" Max {max(fits)}")
-        print(f" Średnia arytmetyczna {mean}")
-        print(f" Średnie odchylenie std {std}")
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
 
-    print("---- ewolucja zakończona powodzeniem ----")
+    print("-- End of (successful) evolution --")
 
-    best_ind = tools.selBest(pop,1)[0]
-    print(f"Najlepszy osobnik: {best_ind}, {best_ind.fitnes.values}")
+    best_ind = tools.selBest(pop, 1)[0]
+    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
+
