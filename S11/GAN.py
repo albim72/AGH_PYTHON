f422a5e01acbb64f25b4bf10e1730cc488e66b48
albim72 <marcin.albiniak72@gmail.com>
2023-01-10 17:59:38 +0100

    Update GAN.py

--- S11/GAN.py
+++ S11/GAN.py
@@ -4,3 +4,14 @@ import tensorflow as tf
 
 !pip install git+https://github.com/tensorflow/docs
   
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers
+import time
+
+from IPython import display
+
