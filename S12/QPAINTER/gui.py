ffcdf50304a034f14bce9a3a249bfeaee7b560c4
albim72 <marcin.albiniak72@gmail.com>
2023-01-13 18:21:17 +0100

    Update gui.py

--- S12/QPAINTER/gui.py
+++ S12/QPAINTER/gui.py
@@ -1,33 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import QRect
+from ksztalty import Ksztalty, Ksztalt
+from PyQt5.QtWidgets import QHBoxLayout
+
 
 class Ui_Widget(object):
+    """ Klasa definiująca GUI """
 
-    def setupUi(self,Widget):
-        self.ksztalt = Ksztalty.Ellipse
-        self.prost = QRect(1,1,101,101)
-        self.kolorO = QColor(0,0,0)
-        self.kolorW = QColor(200,30,40)
+    def setupUi(self, Widget):
 
-        self.resize(102,102)
+        # widget rysujący kształty, instancja klasy Ksztalt
+        self.ksztalt = Ksztalt(self, Ksztalty.Polygon)
+
+        # układ poziomy, zawiera: self.ksztalt
+        ukladH1 = QHBoxLayout()
+        ukladH1.addWidget(self.ksztalt)
+
+        self.setLayout(ukladH1)  # przypisanie układu do okna głównego
         self.setWindowTitle('Widżety')
-
-    def paintEvent(self,e):
-        qp = QPainter()
-        qp.begin(self)
-        self.rysujFigury(e,qp)
-        qp.end()
-
-    def rysujFigury(self,e,qp):
-        qp.setPen(self.kolorO)
-        qp.setBrush(self.kolorW)
-        qp.setRenderHint(QPainter.Antialiasing)
-        if self.ksztalt == Ksztalty.Rect:
-            qp.drawRect(self.prost)
-        elif self.ksztalt == Ksztalty.Ellipse:
-            qp.drawEllipse(self.prost)
-
-
-class Ksztalty:
-    Rect,Ellipse,Polygon,Line = range(4)
