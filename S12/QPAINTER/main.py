559f49c4e108c54516c85de6509b2b51af27d1a5
albim72 <marcin.albiniak72@gmail.com>
2023-01-13 18:20:12 +0100

    Update main.py

--- S12/QPAINTER/main.py
+++ S12/QPAINTER/main.py
@@ -1,15 +1,23 @@
-from __future__ import unicode_literals
-from PyQt5.QtWidgets import QApplication,QWidget
-from gui import Ui_Widget
-import sys
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-class Widgety(QWidget,Ui_Widget):
-    def __init__(self,parent=None):
-        super(Widgety,self).__init__(parent)
-        self.setupUi(self)
+from __future__ import unicode_literals
+from PyQt5.QtWidgets import QApplication, QWidget
+from gui_z1 import Ui_Widget
+
+
+class Widgety(QWidget, Ui_Widget):
+    """ Główna klasa aplikacji """
+
+    def __init__(self, parent=None):
+        super(Widgety, self).__init__(parent)
+        self.setupUi(self)  # tworzenie interfejsu
 
 if __name__ == '__main__':
+    import sys
+
     app = QApplication(sys.argv)
     okno = Widgety()
     okno.show()
+
     sys.exit(app.exec_())
