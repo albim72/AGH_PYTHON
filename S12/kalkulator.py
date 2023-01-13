4a010ce5db8aea1143da1513e4621b6917a56ee7
albim72 <marcin.albiniak72@gmail.com>
2023-01-13 16:25:55 +0100

    Update kalkulator.py

--- S12/kalkulator.py
+++ S12/kalkulator.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QGridLayout
+from PyQt5.QtGui import QIcon
 import sys
 
 class Kalkulator(QWidget):
@@ -7,8 +8,25 @@ class Kalkulator(QWidget):
         self.interfejs()
 
     def interfejs(self):
-        self.resize(300,100)
+
+        #etykiety
+        etykieta1 = QLabel("liczba a:",self)
+        etykieta2 = QLabel("liczba b:",self)
+        etykieta3 = QLabel("wynik:",self)
+
+        #przypisanie widgetów do układu tabelarycznego
+        ukladT = QGridLayout()
+        ukladT.addWidget(etykieta1,0,0)
+        ukladT.addWidget(etykieta2,0,1)
+        ukladT.addWidget(etykieta3,0,2)
+
+        #przypisanie utworzonego układu do okna
+        self.setLayout(ukladT)
+        self.setGeometry(20,20,300,100)
+        self.setWindowIcon(QIcon('name.jpg'))
         self.setWindowTitle("Prosty kalkulator")
+        # self.resize(300,100)
+        # self.setWindowTitle("Prosty kalkulator")
         self.show()
 
 if __name__ == '__main__':
