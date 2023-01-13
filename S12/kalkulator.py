ebcb265c290a047dc942f5207f446f09b2e9ec1e
albim72 <marcin.albiniak72@gmail.com>
2023-01-13 16:46:52 +0100

    Update kalkulator.py

--- S12/kalkulator.py
+++ S12/kalkulator.py
@@ -12,6 +12,21 @@ class Kalkulator(QWidget):
     def koniec(self):
         self.close()
 
+    def closeEvent(self, event) -> None:
+        odp = QMessageBox.question(
+            self,'Komunikat',
+            "Czy na pewno koniec?",
+            QMessageBox.Yes | QMessageBox.No, QMessageBox.No
+        )
+        if odp == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def keyPressEvent(self, e) -> None:
+        if e.key() == Qt.Key_Escape:
+            self.close()
+
     def interfejs(self):
 
         #etykiety
